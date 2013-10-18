691867cf60abd109122a9915ea68484cc178b11d
janusz buda <janusz@jsbdigital.com>
2013-10-18 12:29:20 +0100

    Ruby day 1

---
+++ ruby/day1.rb
@@ -0,0 +1,25 @@
+# output "Hello world"
+
+puts "Hello world"
+
+
+# find the index of the world "Ruby"
+
+str = "Hello, Ruby"
+idx = str.index("Ruby")
+puts "Index of 'Ruby' is : #{idx}"
+
+
+# output name 10 times
+
+10.times do
+  puts "janusz"
+end
+
+
+# output sentence 10 times incrementing the iterator from 1-10
+
+10.times do |idx|
+  puts "This is the sentence number #{idx+1}"
+end
+
