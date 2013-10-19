53c5f334497ee3438a6b1afe1e4aa464b716b08a
janusz buda <janusz@jsbdigital.com>
2013-10-19 22:04:41 +0100

    Ruby 2 exercise 2

--- ruby/day2.rb
+++ ruby/day2.rb
@@ -12,8 +12,39 @@ values = []
   end
 end
 
+puts
+
 # using each_slice
 
 [*(1..16)].each_slice(4) do |i|
   puts i.join(",")
 end
+
+puts
+
+# update the tree initialiser to accept a nested structure with hashes and arrays
+class Tree
+  attr_accessor :children, :node_name
+  
+  def initialize(tree)
+    
+    @node_name = tree.keys()[0]
+    @children = []    
+
+    tree[tree.keys()[0]].each do |k,v|
+      @children.push(Tree.new({k=>v}))
+    end
+  end
+  
+  def visit_all(&block)
+    visit &block
+    children.each { |c| c.visit_all &block }
+  end
+
+  def visit(&block)
+    block.call self
+  end
+end
+
+tree = Tree.new({'grandpa'=>{'dad'=>{'child 1'=>{},'child 2'=>{}},'uncle'=>{'child 3'=>{},'child 4'=>{}}}})
+tree.visit_all {|node| puts node.node_name }
