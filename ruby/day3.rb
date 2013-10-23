ebc3f332e1d911853f155f8a6d1fe755c4f4a83e
janusz buda <janusz@jsbdigital.com>
2013-10-23 17:47:55 +0100

    Ruby day 3

--- ruby/day3.rb
+++ ruby/day3.rb
@@ -1,5 +1,20 @@
 module ActsAsCsv
 
+  class CsvRow
+    
+    attr_accessor :header, :content
+
+    def initialize header,content
+      @header = header
+      @content = content
+    end
+    
+    def method_missing name, *args
+      @content[@header.find_index(name.to_s)]
+    end
+
+  end
+
   def self.included(base)
     base.extend ClassMethods
   end
@@ -16,10 +31,16 @@ module ActsAsCsv
       @csv_contents = []
       filename = self.class.to_s.downcase+'.txt'
       file = File.new(filename)
-      @headers = file.gets.chomp.split(', ')
+      @headers = file.gets.chomp.split(',')
       
       file.each do |row|
-        @csv_contents << row.chomp.split(', ')
+        @csv_contents << CsvRow.new(@headers,row.chomp.split(','))
+      end
+    end
+    
+    def each 
+      @csv_contents.each do |row|
+        yield row
       end
     end
 
@@ -30,3 +51,14 @@ module ActsAsCsv
     end
   end
 end
+
+class RubyCsv
+  include ActsAsCsv
+  acts_as_csv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_contents.inspect
+
+m.each { |row| puts "#{row.name} is #{row.age} years old" }
